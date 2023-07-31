f85a0ca60ab1b91602ec0c313180289bc70068dd
JungSooKim <61766408+dmdm2002@users.noreply.github.com>
2023-07-31 15:50:54 +0900

    Add files via upload

---
+++ DenseNet.py
@@ -0,0 +1,68 @@
+import torch
+import torch.nn as nn
+
+from Model.Modules.Layers import BottleNeck, Transition
+
+
+# DenseNet
+class DenseNet(nn.Module):
+    def __init__(self, nblocks, growth_rate=12, reduction=0.5, num_classes=10, init_weights=True):
+        super().__init__()
+
+        self.growth_rate = growth_rate
+        inner_channels = 2 * growth_rate  # output channels of conv1 before entering Dense Block
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, inner_channels, 7, stride=2, padding=3),
+            nn.MaxPool2d(3, 2, padding=1)
+        )
+
+        self.features = nn.Sequential()
+
+        for i in range(len(nblocks) - 1):
+            self.features.add_module('dense_block_{}'.format(i), self._make_dense_block(nblocks[i], inner_channels))
+            inner_channels += growth_rate * nblocks[i]
+            out_channels = int(reduction * inner_channels)
+            self.features.add_module('transition_layer_{}'.format(i), Transition(inner_channels, out_channels))
+            inner_channels = out_channels
+
+        self.features.add_module('dense_block_{}'.format(len(nblocks) - 1),
+                                 self._make_dense_block(nblocks[len(nblocks) - 1], inner_channels))
+        inner_channels += growth_rate * nblocks[len(nblocks) - 1]
+        self.features.add_module('bn', nn.BatchNorm2d(inner_channels))
+        self.features.add_module('relu', nn.ReLU())
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.linear = nn.Linear(inner_channels, num_classes)
+
+        # weight initialization
+        if init_weights:
+            self._initialize_weights()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.features(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear(x)
+        return x
+
+    def _make_dense_block(self, nblock, inner_channels):
+        dense_block = nn.Sequential()
+        for i in range(nblock):
+            dense_block.add_module('bottle_neck_layer_{}'.format(i), BottleNeck(inner_channels, self.growth_rate))
+            inner_channels += self.growth_rate
+        return dense_block
+
+
+def DenseNet_121():
+    return DenseNet([6, 12, 24, 16])
+
+def DenseNet_169():
+    return DenseNet([6, 12, 32, 32])
+
+def DenseNet_201():
+    return DenseNet([6, 12, 48, 32])
+
+def DenseNet_264():
+    return DenseNet([6, 12, 64, 48])
---
+++ Modules/Layers.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+
+class BottleNeck(nn.Module):
+    def __init__(self, in_channels, growth_rate):
+        super().__init__()
+        inner_channels = 4 * growth_rate
+
+        self.residual = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, inner_channels, 1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(inner_channels),
+            nn.ReLU(),
+            nn.Conv2d(inner_channels, growth_rate, 3, stride=1, padding=1, bias=False)
+        )
+
+        self.shortcut = nn.Sequential()
+
+    def forward(self, x):
+        return torch.cat([self.shortcut(x), self.residual(x)], 1)
+
+    
+class Transition(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.down_sample = nn.Sequential(
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+            nn.Conv2d(in_channels, out_channels, 1, stride=1, padding=0, bias=False),
+            nn.AvgPool2d(2, stride=2)
+        )
+
+    def forward(self, x):
+        return self.down_sample(x)
